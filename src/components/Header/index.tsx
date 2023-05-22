e34d34bce05f015b64d6888db3f6d131f5851409
Dante Vicenzo <dantevcz@gmail.com>
2023-05-22 12:02:26 -0300

    styled header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import { CartLink, HeaderContainer, SetLocationLink } from './styles'
+import logo from '../../assets/logo.svg'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+
 export function Header() {
-  return <header>Header</header>
+  return (
+    <HeaderContainer>
+      <img src={logo} />
+      <nav>
+        <SetLocationLink href="/">
+          <MapPin size={24} weight="fill" />
+          Salvador, BA
+        </SetLocationLink>
+        <CartLink href="/checkout">
+          <ShoppingCart size={24} weight="fill" />
+        </CartLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 2rem 0;
+
+  nav {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+
+    a {
+      padding: 0.5rem;
+      border-radius: ${(props) => props.theme.borderRadius};
+      display: flex;
+      align-items: center;
+      gap: 0.25rem;
+
+      text-decoration: none;
+
+      transition: color, background-color 0.15s;
+    }
+  }
+`
+
+export const SetLocationLink = styled.a`
+  color: ${(props) => props.theme.color.purpleDark};
+  background-color: ${(props) => props.theme.color.purpleLight};
+
+  svg {
+    color: ${(props) => props.theme.color.purple};
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: ${(props) => props.theme.color.purpleLight};
+    background-color: ${(props) => props.theme.color.purpleDark};
+  }
+`
+export const CartLink = styled.a`
+  background-color: ${(props) => props.theme.color.yellowLight};
+
+  svg {
+    color: ${(props) => props.theme.color.yellowDark};
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) => props.theme.color.yellowDark};
+
+    svg {
+      color: ${(props) => props.theme.color.yellowLight};
+    }
+  }
+`
